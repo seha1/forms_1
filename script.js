10f5a54f5a928a9f3eca308e48afcb5b89951b32
seha1 <seha1@yandex.ru>
2021-05-02 11:18:51 +0300

    fix: writed logic

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 ;
 document.addEventListener('DOMContentLoaded', () => {
-    let subBtn = document.getElementById('submit');
     let form = document.querySelectorAll('.form');
-
 
     form[0].addEventListener('submit', e => {
         e.preventDefault();
 
-        const body = new FormData(form[0]);
+        let arrFormData = {};
+
+        for(let i = 0; i < form[0].length; i++){
+            let name = form[0][i].name;
+            let value = form[0][i].value;
+
+            arrFormData[name] = value;
+        }
+        let body = JSON.stringify(arrFormData);
 
         fetch(form[0].action, {
             method: 'POST',
-            body
+            body,
         }).then(response => {
             console.log(response);
         }).catch(() => {
